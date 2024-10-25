93879dec005b5ae365f0bd591386f47bf1fd3f37
f.lahalle <f.lahalle@student.isartdigital.com>
2024-10-25 14:55:51 +0200

    Init Project

---
+++ Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        Socket socket;
+        int port = 11000;
+        public bool Connected { get { return socket.Connected; } }
+        public Client()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAdress = host.AddressList[0];
+            socket = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Connect()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAdress = host.AddressList[0];
+            IPEndPoint serverEP = new IPEndPoint(ipAdress, 11000);
+            try
+            {
+                socket.Connect(serverEP);
+                Console.WriteLine("Connected to server at " + ipAdress.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error connecting to server " + e.ToString());
+                Disconnect();
+            }
+        }
+        public void Disconnect()
+        {
+            if (socket != null)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        }
+        public void SendMessage(string message)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+            try
+            {
+                socket.Send(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error sending message : " + e.ToString());
+            }
+        }
+        public string ReceiveMessage()
+        {
+            try
+            {
+                byte[] messageReceived = new byte[1024];
+                int nbBytes = socket.Receive(messageReceived);
+                return Encoding.ASCII.GetString(messageReceived, 0, nbBytes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error receiving message : " + e.ToString());
+            }
+            return String.Empty;
+        }
+    }
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Connect();
+
+            string message = "Hello from Client";
+            client.SendMessage(message);
+            Console.WriteLine("client has sent message " + message);
+
+            message = client.ReceiveMessage();
+            Console.WriteLine("client has received message " + message);
+
+            client.Disconnect();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP_UDP_TEST/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        Socket socket;
+        int port = 11000;
+        IPEndPoint localEP;
+        Socket clientSocket;
+        public Server()
+        {
+            //create server socket
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAdress = host.AddressList[0];
+            localEP = new IPEndPoint(ipAdress, port);
+            socket = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting Server");
+            socket.Bind(localEP);
+            socket.Listen(10);
+
+            try
+            {
+                Console.WriteLine("Waiting for a connection...");
+                // blocking instruction
+                clientSocket = socket.Accept();
+
+                Console.WriteLine("Accepted Client !");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error " + e.ToString());
+                Disconnect();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (clientSocket != null)
+            {
+                // shutdown client socket
+                try
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("error " + e.ToString());
+                }
+                finally
+                {
+                    clientSocket.Close();
+                }
+            }
+
+            if (socket != null)
+            {
+                // server socket : no shutdown necessary
+                socket.Close();
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+            try
+            {
+                clientSocket.Send(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error sending message : " + e.ToString());
+            }
+        }
+
+        public string ReceiveMessage()
+        {
+            try
+            {
+                byte[] messageReceived = new byte[1024];
+                int nbBytes = clientSocket.Receive(messageReceived);
+                return Encoding.ASCII.GetString(messageReceived, 0, nbBytes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error receiving message : " + e.ToString());
+            }
+            return String.Empty;
+        }
+    }
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start();
+
+            string message = server.ReceiveMessage();
+            Console.WriteLine("Server has received message : " + message);
+
+            server.SendMessage("Hello from Server");
+
+            server.Disconnect();
+            Console.ReadKey();
+        }
+    }
+}
